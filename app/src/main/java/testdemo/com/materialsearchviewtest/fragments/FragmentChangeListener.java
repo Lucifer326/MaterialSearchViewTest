2561f391918e62f26c246f639111cdc2a34aba02
emnrd_ito <john.daues.emnrd@gmail.com>
2017-03-01 07:06:40 -0700

    Initialize

---
+++ app/src/main/java/testdemo/com/materialsearchviewtest/fragments/FragmentChangeListener.java
@@ -0,0 +1,14 @@
+package testdemo.com.materialsearchviewtest.fragments;
+
+
+import android.content.Context;
+import android.support.v4.app.FragmentManager;
+
+public interface FragmentChangeListener {
+    void onFragmentChange(Context context,
+                          FragmentChangeEvent fragmentChangeEvent,
+                          final FragmentManager fragmentManager,
+                          String tag);
+    //boolean onFragmentPop(FragmentChangeEvent fragmentChangeEvent);
+
+}
